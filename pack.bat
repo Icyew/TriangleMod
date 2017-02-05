8b5b845f1e2d6bfe9df9a066f25e67e8ced666c9
TriangleAuthority <adr.sierra@gmail.com>
2017-02-04 16:23:49 -0800

    added and updated some utility files from master

--- pack.bat
+++ pack.bat
@@ -1,27 +1,6 @@
 cd "C:\W3Mods\Witcher_3_Mod_Tools\bin\x64"
-setlocal
-set modName=mod1111Triangle
-set unpackedPath=C:\W3mods\TriangleMod\temp\Witcher3
-
-rmdir "C:\GOG Games\The Witcher 3 Wild Hunt\Mods\%modName%\content\scripts" /S/Q
-xcopy "%unpackedPath%\content\scripts" "C:\GOG Games\The Witcher 3 Wild Hunt\Mods\%modName%\content\scripts" /S/K/I/Y
-rmdir "%unpackedPath%\content\scripts" /S/Q
-xcopy "%unpackedPath%\content\content0" "C:\GOG Games\The Witcher 3 Wild Hunt\Mods\%modName%\content\content0" /S/K/I/Y
-rmdir "%unpackedPath%\content\content0" /S/Q
-
-set isEmpty=true
-for /F %%i in ('dir /b /a "%unpackedPath%\content\*"') do (
-    if not "%%i" == "scripts" (
-    	if not "%%i" == "content0" (
-    		set isEmpty=false
-    	)
-    )
-)
-
-if "%isEmpty%" == "false" (
-	wcc_lite pack -dir="%unpackedPath%\content" -outdir="C:\GOG Games\The Witcher 3 Wild Hunt\Mods\%modName%\content"
-	wcc_lite metadatastore -path="C:\GOG Games\The Witcher 3 Wild Hunt\Mods\%modName%\content"
-)
+wcc_lite pack -dir="C:\W3mods\TMod\temp\Witcher3\content" -outdir="C:\GOG Games\The Witcher 3 Wild Hunt\Mods\mod1111Triangle\content"
+wcc_lite metadatastore -path="C:\GOG Games\The Witcher 3 Wild Hunt\Mods\mod1111Triangle\content"
 
 
 
